c4ae8962aeb3055dd34dfa5bda73692fd533bd79
IrfanSaeednarejo <irfansaeed.narejo@gmail.com>
2025-07-02 17:52:54 +0500

    conected to DB

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Brand Pilot Backend is running...");
+import dotenv from "dotenv"
+
+import express from "express"
+import mongoose from "mongoose"
+
+dotenv.config({
+    path: './.env'
+})
+
+const app = express()
+
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+
+// app.use('/api/user', userRoutes)
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+  
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
