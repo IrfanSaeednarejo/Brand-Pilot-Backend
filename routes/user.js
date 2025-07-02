2edb7057af003614448feeda07c8db2713ac9a95
IrfanSaeednarejo <irfansaeed.narejo@gmail.com>
2025-07-02 18:00:34 +0500

    controller and Routes written

---
+++ routes/user.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+
+import { loginUser, signupUser } from '../controllers/userController'
+
+const router = express.Router()
+
+
+router.post('/login', loginUser)
+
+
+router.post('/signup', signupUser)
+
+module.exports = router
